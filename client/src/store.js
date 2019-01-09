6a01ca9a50dcf942dd678c9716a328ffc4aa491d
‘Tim <timothy.j.ocampo@gmail.com>
2019-01-08 21:23:11 -0800

    does not show on mobile. Have to edit compose( __REDUX__DEVTOOLS__EXTENSION__ on window .see https://github.com/zalmoxisus/redux-devtools-extension/issues/320

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const initialState={};
 const store = createStore(
     rootReducer , initialState ,
     compose(applyMiddleware(...middleware), compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     )));
 
 export default store;
